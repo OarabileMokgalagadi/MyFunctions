7c914ac1b63dec7cb8120220fffe6bd5c9ebca8a
Oarabile Mokgalagadi <Oarabile@explore-datascience.net>
2019-03-19 15:27:27 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Hackathon Project',
+    name='hackathon_project',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='My hackathon project',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<oarabile96>/<Hackathon Project>',
+    url='https://github.com/<oarabile96>/<hackathon_project>',
     author='<Oarabile Mokgalagadi>',
     author_email='<oarabilemokgalagadi@g@gmail.com>'
 )
